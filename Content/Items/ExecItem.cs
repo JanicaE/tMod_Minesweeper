2cd49e7979381a8e971eeef9088625a11483f8a6
JanikaE <2272253314@qq.com>
2025-02-02 17:23:32 +0800

    自由框选区域：预览与生成

--- Content/Items/ExecItem.cs
+++ Content/Items/ExecItem.cs
@@ -17,10 +17,13 @@ namespace Minesweeper.Content.Items
 {
     internal class ExecItem : ModItem
     {
-        private int mapWidth;
-        private int mapHeight;
-        private int mineNum;
+        #region Settings
+        private int MapWidth(Player player) => player.GetModPlayer<MinePlayer>().MapWidth;
+        private int MapHeight(Player player) => player.GetModPlayer<MinePlayer>().MapHeight;
+        private int MineNum(Player player) => player.GetModPlayer<MinePlayer>().MineNum;
+        private int MineDensity(Player player) => player.GetModPlayer<MinePlayer>().MineDensity;
         //private bool breakable;
+        #endregion
 
         private bool cancelled;
         private Point freeStart;
@@ -54,6 +57,23 @@ namespace Minesweeper.Content.Items
                         //Main.NewText("Cancel");
                     }
                     freeEnd = Main.MouseWorld.ToTileCoordinates();
+
+                    int x = Math.Min(freeStart.X, freeEnd.X);
+                    int y = Math.Min(freeStart.Y, freeEnd.Y);
+                    int width = Math.Abs(freeEnd.X - freeStart.X) + 1;
+                    int height = Math.Abs(freeEnd.Y - freeStart.Y) + 1;
+
+                    // 绘制预览
+                    if (cancelled == false)
+                    {                        
+                        Rectangle rectangle = new(x, y, width, height);
+                        DrawPreview(player, rectangle);
+                    }
+                    else
+                    {
+                        DrawPreviewSystem.Clear();
+                    }
+
                     if (Main.mouseLeft)
                     {
                         player.itemAnimation = 8;
@@ -63,7 +83,10 @@ namespace Minesweeper.Content.Items
                         player.itemAnimation = 0;
                         if (cancelled == false)
                         {
-                            Main.NewText($"Generate: ({freeStart.X},{freeStart.Y}),({freeEnd.X},{freeEnd.Y})");
+                            //Main.NewText($"Generate: ({freeStart.X},{freeStart.Y}),({freeEnd.X},{freeEnd.Y})");
+                            int mineNum = (width * height) * MineDensity(player) / 100;
+                            GenerateMap(player, x, y, width, height, mineNum);
+                            DrawPreviewSystem.Clear();
                         }
                     }
                 }
@@ -126,41 +149,14 @@ namespace Minesweeper.Content.Items
             {
                 int x = (int)Main.MouseWorld.X / 16;
                 int y = (int)Main.MouseWorld.Y / 16;
-                Rectangle rectangle = new(x, y, mapWidth, mapHeight);
-                // 无物块区域的预览样式
-                Texture2D textureT = GetTexture("Normal_prev").Value;
-                // 有物块区域的预览样式
-                Texture2D textureF;
-                if (!Config.BlockBreakable)
-                {
-                    textureF = GetTexture("Unbreakable_prev").Value;
-                }
-                else
-                {
-                    textureF = GetTexture("Breakable_prev").Value;
-                }
-                // 如果开启预览，就生成一个Box对象，对应绘制一个方框
-                if (player.GetModPlayer<MinePlayer>().Preview)
-                {
-                    DrawPreviewSystem.NewBox(textureT, textureF, rectangle);
-                }
-                // 反之则清空Box对象
-                else
-                {
-                    DrawPreviewSystem.Clear();
-                }
+                Rectangle rectangle = new(x, y, MapWidth(player), MapHeight(player));
+                DrawPreview(player, rectangle);
             }
         }
 
         public override void UpdateInventory(Player player)
         {
-            // 实时从玩家处更新参数
-            mapWidth = player.GetModPlayer<MinePlayer>().MapWidth;
-            mapHeight = player.GetModPlayer<MinePlayer>().MapHeight;
-            mineNum = player.GetModPlayer<MinePlayer>().MineNum;
-            //breakable = player.GetModPlayer<MinePlayer>().Breakable;
-
-            // 当前没有手持该物品时也清空Box对象
+            // 当前没有手持该物品时清空Box对象
             if (Main.LocalPlayer.HeldItem.type != Type)
             {
                 DrawPreviewSystem.Clear();
@@ -183,38 +179,17 @@ namespace Minesweeper.Content.Items
                 .Register();
         }
 
-        /// <summary>
-        /// 在设置为Unbreakable的情况下，判断区域内是否有mod相关物块以外的物块
-        /// </summary>
-        private bool HasTile()
-        {
-            int x = (int)Main.MouseWorld.X / 16;
-            int y = (int)Main.MouseWorld.Y / 16;
-            for (int i = 0; i < mapWidth; i++)
-            {
-                for (int j = 0; j < mapHeight; j++)
-                {
-                    if (Main.tile[x + i, y + j].HasTile && !MineTiles.Contains(Main.tile[x + i, y + j].TileType) && !Config.BlockBreakable)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
+        #region 私有方法
 
         private void GenerateFixedMap(Player player)
         {
             int x = (int)Main.MouseWorld.X / 16;
             int y = (int)Main.MouseWorld.Y / 16;
+            int mapWidth = MapWidth(player);
+            int mapHeight = MapHeight(player);
+            int mineNum = MineNum(player);
 
             // 判定在某些条件下生成失败
-            // 设置为不可破坏地形且目标区域内存在除扫雷以外的物块时                
-            if (HasTile())
-            {
-                Main.NewText(Language.GetText("Mods.Minesweeper.Items.ExecItem.MainText.False"), new Color(255, 0, 0));
-                return;
-            }
             // 宽或高为0时
             if (mapWidth * mapHeight == 0)
             {
@@ -226,6 +201,38 @@ namespace Minesweeper.Content.Items
             {
                 Main.NewText(Language.GetText("Mods.Minesweeper.Items.ExecItem.MainText.Over"), new Color(255, 0, 0));
                 return;
+            }
+
+            GenerateMap(player, x, y, mapWidth, mapHeight, mineNum);
+            return;
+        }
+
+        private void GenerateMap(Player player, int x, int y, int mapWidth, int mapHeight, int mineNum)
+        {
+            // 设置为不可破坏地形且目标区域内存在除扫雷以外的物块时
+            if (!Config.BlockBreakable)
+            {
+                bool hasTile = false;
+                for (int i = 0; i < mapWidth; i++)
+                {
+                    for (int j = 0; j < mapHeight; j++)
+                    {
+                        if (Main.tile[x + i, y + j].HasTile && !MineTiles.Contains(Main.tile[x + i, y + j].TileType))
+                        {
+                            hasTile = true;
+                            break;
+                        }
+                    }
+                    if (hasTile)
+                    {
+                        break;
+                    }
+                }
+                if (hasTile)
+                {
+                    Main.NewText(Language.GetText("Mods.Minesweeper.Items.ExecItem.MainText.False"), new Color(255, 0, 0));
+                    return;
+                }
             }
 
             // 先生成一片空白区域
@@ -287,7 +294,39 @@ namespace Minesweeper.Content.Items
                     MinesCount(x + mapWidth, i);
                 }
             }
-            return;
         }
+
+        /// <summary>
+        /// 绘制预览
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="rectangle"></param>
+        private void DrawPreview(Player player, Rectangle rectangle)
+        {
+            // 无物块区域的预览样式
+            Texture2D textureT = GetTexture("Normal_prev").Value;
+            // 有物块区域的预览样式
+            Texture2D textureF;
+            if (!Config.BlockBreakable)
+            {
+                textureF = GetTexture("Unbreakable_prev").Value;
+            }
+            else
+            {
+                textureF = GetTexture("Breakable_prev").Value;
+            }
+            // 如果开启预览，就生成一个Box对象，对应绘制一个方框
+            if (player.GetModPlayer<MinePlayer>().Preview)
+            {
+                DrawPreviewSystem.NewBox(textureT, textureF, rectangle);
+            }
+            // 反之则清空Box对象
+            else
+            {
+                DrawPreviewSystem.Clear();
+            }
+        }
+
+        #endregion
     }
 }
